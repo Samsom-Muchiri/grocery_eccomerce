18a7f7466c29084f1ec954daa02d1a5a042b8b19
Sam .M <you@example.com>
2024-04-04 12:15:27 +0300

    added product view page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter } from "react-router-dom";
 import StateChecker from "./StateChecker";
 import LandingPage from "./Public/LandingPage";
 import CheckoutPage from "./Public/CheckoutPage";
+import ProductView from "./Public/ProductView";
 
 function Main() {
   const routes = createBrowserRouter(
@@ -12,6 +13,7 @@ function Main() {
       <Route path="/" element={<StateChecker />}>
         <Route path="/" element={<Nav />}>
           <Route index element={<LandingPage />} />
+          <Route path="/product/:view" element={<ProductView />} />
           <Route path="chekout" element={<CheckoutPage />} />
         </Route>
       </Route>
--- client/src/components/Public/ProductView.jsx
+++ client/src/components/Public/ProductView.jsx
@@ -1,7 +1,163 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
+import { productData } from "../../fakeData";
+import { useParams } from "react-router";
+import "../../styles/productview.css";
+import { CONT } from "../../AppContext/context";
+import { Link } from "react-router-dom";
 
 function ProductView() {
-  return <div></div>;
+  const vl = useContext(CONT);
+  const { view } = useParams();
+  const productId = view.split("_")[1];
+  const product = productData.filter((product) => product.id === productId)[0];
+  console.log(product);
+  const { id, name, image, price, discount, description } = product;
+
+  const ProductSection = ({ products, title }) => {
+    return (
+      <section className="product-section">
+        <h2>{title}</h2>
+        <div className="product-s-row">
+          <div className="psr-slide-btn" style={{ left: "0.5rem" }}>
+            <span className="material-symbols-outlined">arrow_back_ios</span>
+          </div>
+          <div className="psr-cnt">
+            <div className="psr-cnt-w">
+              {products.map((product, i) => {
+                const { name, id, price, discount, image } = product;
+
+                return (
+                  <Fragment key={product.name + i}>
+                    <div className="product-card">
+                      <Link to={`/product/${product.name}_${product.id}`}>
+                        <img src={image} alt="" />
+                      </Link>
+                      <div className="product-detail">
+                        <span className="product-name">{name}</span>
+                        <ul className="product-prices">
+                          <li>
+                            <strike>{vl.formatCurrencyKE(discount)}</strike>
+                          </li>
+                          <li>{vl.formatCurrencyKE(price)}</li>
+                        </ul>
+                      </div>
+                      {vl.cartData.some(
+                        (item) => item.id === id && item.name === name
+                      ) ? (
+                        <ul className="cart-t-q">
+                          <li
+                            onClick={() => {
+                              const targetIndex = vl.cartData.findIndex(
+                                (item) => item.id === id && item.name === name
+                              );
+                              if (targetIndex !== -1) {
+                                const cartData = [...vl.cartData];
+                                cartData[targetIndex] = {
+                                  ...cartData[targetIndex],
+                                  quantity: Math.max(
+                                    cartData[targetIndex].quantity - 1,
+                                    1
+                                  ), // Ensure quantity is at least 1
+                                };
+                                vl.setCartData(cartData);
+                              }
+                            }}
+                          >
+                            -
+                          </li>
+                          <li>
+                            {
+                              vl.cartData.find(
+                                (item) => item.id === id && item.name === name
+                              )?.quantity
+                            }
+                          </li>
+                          <li
+                            onClick={() => {
+                              const targetIndex = vl.cartData.findIndex(
+                                (item) => item.id === id && item.name === name
+                              );
+                              if (targetIndex !== -1) {
+                                const cartData = [...vl.cartData];
+                                cartData[targetIndex] = {
+                                  ...cartData[targetIndex],
+                                  quantity: cartData[targetIndex].quantity + 1,
+                                };
+                                vl.setCartData(cartData);
+                              }
+                            }}
+                          >
+                            +
+                          </li>
+                        </ul>
+                      ) : (
+                        <button
+                          className="cart-add-btn"
+                          onClick={() =>
+                            vl.setCartData((prev) => [
+                              ...prev,
+                              { ...product, quantity: 1 },
+                            ])
+                          }
+                        >
+                          ADD{" "}
+                          <span className="material-symbols-outlined">
+                            shopping_cart
+                          </span>
+                        </button>
+                      )}
+                    </div>
+                  </Fragment>
+                );
+              })}
+            </div>
+          </div>
+          <div className="psr-slide-btn" style={{ right: "0.5rem" }}>
+            <span className="material-symbols-outlined">arrow_forward_ios</span>
+          </div>
+        </div>
+      </section>
+    );
+  };
+
+  const productSections = [
+    {
+      title: "You may also like",
+      data: productData,
+    },
+  ];
+
+  return (
+    <div>
+      <section className="p-v-detail">
+        <div className="pv-image">
+          <img src={image} alt="" />
+        </div>
+        <div className="pv-info">
+          <div className="pv-name">{name}</div>
+          <div className="pv-price">
+            <strike style={{ color: "gray", marginRight: "1rem" }}>
+              {vl.formatCurrencyKE(discount)}
+            </strike>
+            <span>{vl.formatCurrencyKE(price)}</span>
+          </div>
+          <div className="pv-instock">Availability: In stock</div>
+          <button className="pv-addtocart">ADD TO CART</button>
+          <span>Description:</span>
+          <p className="pv-description">{description}</p>
+        </div>
+      </section>
+      <section>
+        {productSections.map((ps, i) => (
+          <ProductSection
+            title={ps.title}
+            products={ps.data}
+            key={ps.title + i}
+          />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default ProductView;
