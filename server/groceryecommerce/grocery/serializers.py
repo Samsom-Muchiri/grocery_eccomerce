590497a0568fe47ae4b6a97bcb6c6c02fdbda1fa
ajackwere <austinewere59@gmail.com>
2024-05-16 18:50:57 +0300

    add serializers for mpesa

--- server/groceryecommerce/grocery/serializers.py
+++ server/groceryecommerce/grocery/serializers.py
@@ -1,43 +1,44 @@
 from rest_framework import serializers
 from .models import Product, Order, Delivery, Cart, MpesaResponseBody, Payment, MobileMoneyPayment
-from MpesaViews.utils import MpesaGateWay
-
-pay = MpesaGateWay()
-
-class SendSTKPushSerializer(serializers.Serializer):
-    phone_payment_number = serializers.CharField()
-    order_id = serializers.IntegerField()
-
-    def validate_order_id(self, value):
-        try:
-            order = Order.objects.get(id=value)
-        except Order.DoesNotExist:
-            raise serializers.ValidationError("Order does not exist")
-        return value
 
 
-    def create(self, validated_data):
-        phone_payment_number = validated_data['phone_payment_number']
-        order_id = validated_data['order_id']
+# class SendSTKPushSerializer(serializers.Serializer):
+#     phone_payment_number = serializers.CharField()
+#     # order_id = serializers.IntegerField()
 
-        try:
-            order = Order.objects.get(id=order_id)
-        except Order.DoesNotExist:
-            raise serializers.ValidationError("Order does not exist")
+#     # def validate_order_id(self, value):
+#     #     try:
+#     #         order = Order.objects.get(id=value)
+#     #     except Order.DoesNotExist:
+#     #         raise serializers.ValidationError("Order does not exist")
+#     #     return value
+
+
+#     def create(self, validated_data):
+#         phone_payment_number = validated_data['phone_payment_number']
+#         # order_id = validated_data['order_id']
+
+#         # try:
+#         #     order = Order.objects.get(id=order_id)
+#         # except Order.DoesNotExist:
+#         #     raise serializers.ValidationError("Order does not exist")
         
-        amount = order.total_price
+#         # amount = order.total_price
+        
 
-        if str(phone_payment_number)[0] == "+":
-            phone_payment_number = phone_payment_number[1:]
-        elif str(phone_payment_number)[0] == "0":
-            phone_payment_number = "254" + phone_payment_number[1:]
+#         amount = 1
 
-        callback_url = ''
-        payment = pay.stk_push(phone_payment_number=phone_payment_number, amount=amount, callback_url=callback_url)
+#         if str(phone_payment_number)[0] == "+":
+#             phone_payment_number = phone_payment_number[1:]
+#         elif str(phone_payment_number)[0] == "0":
+#             phone_payment_number = "254" + phone_payment_number[1:]
 
-        res = payment.json()
+#         callback_url = 'https://cae7-102-213-93-44.ngrok-free.app/mpesa/callback/'
+#         payment = pay.stk_push(phone_payment_number=phone_payment_number, amount=amount, callback_url=callback_url)
 
-        return res
+#         res = payment.json()
+
+#         return res
     
 
 class MpesaResponseBodySerializer(serializers.ModelSerializer):
