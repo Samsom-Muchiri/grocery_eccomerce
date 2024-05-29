74fa7aebee4d53fd2f3fe378c549e386255a023a
ajackwere <austinewere59@gmail.com>
2024-05-29 14:39:52 +0300

    remove csrf for regster

--- server/groceryecommerce/groceryecommerce/settings.py
+++ server/groceryecommerce/groceryecommerce/settings.py
@@ -43,13 +43,20 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
+CSRF_TRUSTED_ORIGINS = []
+
+CSRF_COOKIE_SAMESITE = None
+
 CORS_ALLOW_ALL_ORIGINS = True
+
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:3000',
